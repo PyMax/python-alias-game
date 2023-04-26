74d501375eb45cca4c7514c3457502954b6e57af
maxim_b <qtcreator777@gmail.com>
2023-04-26 22:28:28 +0300

    added host

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def nextWord():
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	socketio.run(app, debug=False, port=port, allow_unsafe_werkzeug=True)
+	socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
