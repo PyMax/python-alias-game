f48f45dbae271b020b1667277b2276bd8b71df0e
PyMax <qtcreator777@gmail.com>
2023-05-10 16:11:48 +0300

    configure gunicorn for socketio

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn -k gevent -w 1 --threads 50 app:app  -b 0.0.0.0:5000 --reload
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --threads 50 app:app  -b 0.0.0.0:5000 --reload
